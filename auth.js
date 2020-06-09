dbf057937ec9183eceed8031e98f5c8f2173fd1a
Yesh Yendamuri <yeshwanth.yk@gmail.com>
2020-06-08 22:02:55 -0400

    Add logic to do spotify auth

---
+++ auth.js
@@ -0,0 +1,62 @@
+const open = require('open');
+const express = require('express');
+const chalk = require('chalk');
+const clipboardy = require('clipboardy');
+
+// @Todo Add a way to pass in clientId
+const PORT = 4815;
+const CLIENT_ID = '';
+const SHOW_DIALOG = false;
+const SCOPE = [
+  'playlist-modify-public',
+  'playlist-modify-private',
+  'user-read-recently-played',
+  'user-read-currently-playing',
+  'playlist-read-private',
+].join('%20');
+
+const REDIRECT_URI = 'http://localhost:' + PORT + '/callback';
+
+const URL =
+  'https://accounts.spotify.com/authorize' +
+  '?client_id=' +
+  CLIENT_ID +
+  '&response_type=token' +
+  '&scope=' +
+  SCOPE +
+  '&show_dialog=' +
+  SHOW_DIALOG +
+  '&redirect_uri=' +
+  REDIRECT_URI;
+
+const app = express();
+
+app.get('/callback', (req, res) => {
+  res.sendFile(__dirname + '/callback.html');
+  if (req.query.error) {
+    console.log(chalk.red('Something went wrong. Error: '), req.query.error);
+  }
+});
+
+app.get('/token', (req, res) => {
+  res.sendStatus(200);
+  const token = req.query.access_token;
+  if (token) {
+    clipboardy.writeSync(token);
+    console.log(chalk.green('Your token is: '), chalk.bold(token));
+    console.log('(It has been copied to your clipboard)');
+  }
+
+  process.exit();
+});
+
+const auth = () => {
+  app.listen(PORT, () => {
+    console.log(
+      chalk.blue('Opening the Spotify Login Dialog in your browser...')
+    );
+    open(URL);
+  });
+};
+
+module.exports = auth;
