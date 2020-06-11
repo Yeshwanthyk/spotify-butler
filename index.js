f2a1197e02ed1fa46572793a7ea486113ac040fa
yeshwanthyk <yeshwanth.yk@gmail.com>
2020-06-10 22:45:24 -0400

    Grab current song

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { program } = require('commander');
-const SpotifyWebApi = require('spotify-web-api-node');
+const fetch = require('node-fetch');
 
 const auth = require('./auth.js');
 
 program
   .option('-o, --oauth', 'Authenticate with Spotify')
-  .option('-t, --token', 'Pass the Auth Token')
+  .option('-t, --auth-token <token>', 'Pass the Auth Token')
   .option('-p, --playlists', 'Show all playlists')
   .option('-a, --add <type>', 'Add current song to playlist of choice');
 
@@ -16,6 +16,32 @@ if (program.oauth) {
 }
 
 if (program.playlists) {
-  spotifyApi.setAccessToken(program.token);
+  if (!program.authToken) return;
 }
-if (program.add) console.log(`- ${program.add}`);
+
+if (program.add) {
+  if (!program.authToken) return;
+
+  const token = program.authToken;
+  getCurrentSong(token).then((songURI) => {
+    console.log('songURI', songURI);
+  });
+}
+
+async function getCurrentSong(token) {
+  const url = 'https://api.spotify.com/v1/me/player/currently-playing';
+
+  const headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${token}`,
+  };
+
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: headers,
+  });
+
+  const data = await response.json();
+  return data.item.uri;
+}
