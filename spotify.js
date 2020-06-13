f1d15f52784963b9d195b699c15433cca38d70b0
yeshwanthyk <yeshwanth.yk@gmail.com>
2020-06-13 13:45:58 -0400

    Use getFetch to grab current song

--- spotify.js
+++ spotify.js
@@ -1,6 +1,16 @@
 const fetch = require("node-fetch");
 
 async function getCurrentSong(token) {
+	const url = "https://api.spotify.com/v1/me/player/currently-playing";
+
+	const response = await fetch(url, {
+		method: "GET",
+		headers: setHeaders(token)
+	});
+
+	const data = await response.json();
+	return data.item.uri;
+}
 async function makeFetch(url, token) {
 	const response = await fetch(url, {
 		method: "GET",
