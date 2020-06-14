28fa894c57db1a1016c8f62b3d5b8291ca22784e
yeshwanthyk <yeshwanth.yk@gmail.com>
2020-06-14 00:29:04 -0400

    Bump up version

--- bin/index.js
+++ bin/index.js
@@ -1,50 +1,50 @@
 #!/usr/bin/env node
 
-const { program } = require("commander");
-const auth = require("./auth/auth.js");
+const { program } = require('commander');
+const auth = require('./auth/auth.js');
 const {
-	getCurrentSong,
-	getPlaylists,
-	addSongToPlaylist
-} = require("./spotify.js");
+  getCurrentSong,
+  getPlaylists,
+  addSongToPlaylist,
+} = require('./spotify.js');
 
 program
-	.option("-o, --oauth", "Authenticate with Spotify")
-	.option("-t, --auth-token <token>", "Pass the Auth Token")
-	.option("-p, --playlists", "Show all playlists")
-	.option("-a, --add <id>", "Add current song to playlist of choice");
+  .option('-o, --oauth', 'Authenticate with Spotify')
+  .option('-t, --auth-token <token>', 'Pass the Auth Token')
+  .option('-p, --playlists', 'Show all playlists')
+  .option('-a, --add <id>', 'Add current song to playlist of choice');
 
 program.parse(process.argv);
 
 if (program.oauth) {
-	auth();
+  auth();
 }
 
 if (program.playlists) {
-	if (!program.authToken) return;
-	const token = program.authToken;
+  if (!program.authToken) return;
+  const token = program.authToken;
 
-	let playlists = {};
+  let playlists = {};
 
-	getPlaylists(token).then(playlistsJson => {
-		playlistsJson.items.forEach(item => {
-			console.log(item.name, item.id);
-		});
-	});
+  getPlaylists(token).then((playlistsJson) => {
+    playlistsJson.items.forEach((item) => {
+      console.log(item.name, item.id);
+    });
+  });
 }
 
 if (program.add) {
-	if (!program.authToken) return;
+  if (!program.authToken) return;
 
-	const playlist_id = program.add;
-	const token = program.authToken;
+  const playlist_id = program.add;
+  const token = program.authToken;
 
-	getCurrentSong(token).then(songId => {
-		if (songId.error) {
-			console.log("OAuth Token expired. Refetching...");
-			auth();
-		} else {
-			addSongToPlaylist(token, playlist_id, songId.item.uri);
-		}
-	});
+  getCurrentSong(token).then((songId) => {
+    if (songId.error) {
+      console.log('OAuth Token expired. Refetching...');
+      auth();
+    } else {
+      addSongToPlaylist(token, playlist_id, songId.item.uri);
+    }
+  });
 }
